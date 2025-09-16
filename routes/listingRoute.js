ec3f7bc4b8b1fe83229133d464ba41e4adf6a948
singhmahipal <mahipalsingh052025@gmail.com>
2025-09-16 19:00:47 +0530

    part 2 phase c

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -33,6 +33,7 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
 
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
     })
 );
@@ -41,6 +42,12 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist.");
+        return res.redirect("/listings");
+    }
+
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -48,13 +55,20 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
+
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist.");
+        return res.redirect("/listings");
+    }
+
     res.render("listings/edit.ejs", {listing});
 }));
 
 //Update route
 router.put("/:id", validateListing, wrapAsync(async (req, res) => {
     const {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});    
+    req.flash("success", "Listing Updated");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -62,7 +76,8 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 router.delete("/:id", wrapAsync(async (req, res) => {
     const {id} = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
+    console.log(deletedListing);    
+    req.flash("success", "Listing Deleted");
     res.redirect("/listings");
 }));
 
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -26,8 +26,9 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
     await newReview.save();
     await listing.save();
 
-    console.log("new review saved");
-    res.redirect(`/listings/${listing._id}`)
+    console.log("new review saved");    
+    req.flash("success", "New Review Created");
+    res.redirect(`/listings/${listing._id}`);
 }));
 
 //Review Delete route
@@ -36,7 +37,7 @@ router.delete("/:reviewId", wrapAsync( async (req, res) => {
 
     await Listing.findByIdAndUpdate(id, { $pull: {reviews: reviewId} });
     await Review.findByIdAndDelete(reviewId);
-
+    req.flash("success", "Review Deleted");
     res.redirect(`/listings/${id}`);
 }));
 
